67c3e27716ee5948e45b286707a35d996adb5be7
PMafra <pmafrarm@gmail.com>
2021-11-24 18:35:48 -0300

    Finish signIn refactor

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,7 +21,31 @@ async function signUp (req, res) {
       }
 }
 
+async function signIn (req, res) {
+    try {
+      const { email, password } = req.body;
+  
+      if (!email || !password) {
+        return res.sendStatus(400);
+      }
+
+      const token = await userService.createUserSession({ email, password });
+
+      if (!token) {
+        return res.sendStatus(401);
+      }
+
+      res.send({
+        token
+      });
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
+  }
+
 
 export {
-    signUp
+    signUp,
+    signIn,
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,6 @@
 import * as userRepository from "../repositories/userRepository.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 async function createUser ({ name, email, password }) {
 
@@ -16,6 +17,22 @@ async function createUser ({ name, email, password }) {
     return true;
 }
 
+async function createUserSession ({ email, password }) {
+
+      const user = await userRepository.selectUser({ email });
+  
+      if (!user.rows[0] || !bcrypt.compareSync(password, user.rows[0].password)) {
+        return null;
+      }
+  
+      const token = jwt.sign({
+        id: user.rows[0].id
+      }, process.env.JWT_SECRET);
+
+      return token;
+}
+
 export {
     createUser,
+    createUserSession,
 }
